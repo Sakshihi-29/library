bb0a764cefec6b7e9e31fb3ff17e6d0335e47de9
Sakshihi-29 <sakshi_25ds096@saitm.ac.in>
2025-11-12 12:27:16 +0530

    Add files via upload

---
+++ 12 nov libsi.py
@@ -0,0 +1,34 @@
+#print("Welcome User")
+def run():
+    print("PRESS 1 For Continue")
+    print("PRESS 2 For Exit")
+    a=int(input("PRESS Number to Continue or Exit"))
+    if a==1:
+        pass
+    elif a==2:
+      print("Thanks For Visting")
+
+run()
+
+
+def book():
+    print("PRESS 1 For ALL THE ROADS LEAD TO ROME")
+    print("PRESS 2 For SCARS")
+    print("PRESS 3 For ANNABELLE")
+    b=int(input("enter book number to continue"))
+    if b==1:
+          print("ALL THE ROADS LEAD TO ROME :-- a consipiracy theory of fictional world")
+
+          run()
+    elif b==2:
+        print("SCARS:--A self help book to love yourself")
+
+        run()
+    elif b==3:
+        print("ANNABELLA:-- A cursed doll")
+
+        run()
+
+book()
+        
+
